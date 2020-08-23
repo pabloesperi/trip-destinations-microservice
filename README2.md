bbf6ddb257bac183acbce8f3cf0dc1eb35d0c22d
pabloesperi <pabloesperi@hotmail.com>
2020-08-23 20:57:46 +0200

    first commit

---
+++ README2.md
@@ -0,0 +1,1 @@
+# trip-destinations-microservice
---
+++ src/main/java/com/travelers/TripDestinatiosMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.travelers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripDestinatiosMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripDestinatiosMicroserviceApplication.class, args);
+	}
+
+}
